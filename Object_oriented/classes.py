dbe813d7aa8c7fcd97869f2a3c2d7065cbffbf6a
Mahesh Kumar <p.r.maheshkumar999@gmail.com>
2023-08-06 20:20:24 +0530

    Dictionary methods

--- Object_oriented/classes.py
+++ Object_oriented/classes.py
@@ -1,11 +1,55 @@
 class MyClass:
-    def __init__(self,name:str,year:int,month:str):
+    def __init__(self,name:str,year:int,month:str,date:str,age:int,country:str,domain:str):
         self.name = name
         self.year = year
         self.month = month
-        print("WELCOME TO OBJECT ORIENTED CODING")
+        self.age=age
+        self.country=country
+        self.date=date
+        self.domain=domain
     def study_plans(self):
-        print(f"My name is {self.name} and Im going to USA on {self.month}-{self.year}")
+        print(f"My name is {self.name} and Im going to USA on {self.date}/{self.month}/{self.year}")
+        if self.year<2025:
+            print("Best option you have opted to go there and do the education Now")
+    def alternative_plans(new):
+        if new.year>2025:
+            print(f"{new.name} please go for switching the job roles it may offer you around 17 LPA")
+        else:
+            print(f"{new.name} you did it never stop working hard for your goal it may take time but you will achieve what you want")
+    def create_dict(self):
+        new_dict={
+            "Name":self.name,
+            "Age":self.age,
+            "Country":self.country
+        }
+
+        #Extracting the keys in the dictionary
+        print(new_dict.keys())
+
+        #Extracting the values in the dictionary
+        print(new_dict.values())
+
+        #Extracting the keys & Values in the dictionary using the items()
+        for i,j in new_dict.items():
+            print(f"{i}:{j}")
+
+        #Copying the dictionaries 1.copy() 2.dict()
+        temp_dict=new_dict.copy()
+        print("Original Dictionary :",new_dict)
+
+        #Updating the dictionary with the new keys & values
+        temp_dict.update({
+            "Domain ":self.domain
+        })
+        print("Temp dictionary after copying :",temp_dict)
+        new_dict=dict(temp_dict)
+
+        new_dict.popitem()
+        new_dict.pop("Country")
+        print(new_dict)
+
+
+
 
 
 #Create an object where we can able to access the methods in side the class
@@ -13,4 +57,3 @@ class MyClass:
 # print(det_name.study_plans("Mahesh Kumar",2024,"December"))
 # print(det_name.study_plans("Vidhya sagar",2026,"August"))
 # print(det_name)---->object name
-
---
+++ dictionary.py
@@ -0,0 +1,8 @@
+personal_details={
+    "Name":"P Mahesh Kumar",
+    "Age":20,
+    "Country":"USA"
+}
+
+print(personal_details.get("Age"))
+print(personal_details.keys())
